bc97f074a7264f1fceea29bb6b7412f4433bb66e
Guilherme-W8 <gtavares739@gmail.com>
2024-09-12 17:54:40 -0300

    Exibindo informações sobre o sistema operacional com module OS

---
+++ 3-Core_Modules/15_osInformation/index.js
@@ -0,0 +1,9 @@
+const OperationalSystem =  require('os');
+
+console.log(OperationalSystem.cpus());
+
+console.log(OperationalSystem.freemem());
+
+console.log(OperationalSystem.homedir());
+
+console.log(OperationalSystem.type());
