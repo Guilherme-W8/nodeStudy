c9c0d72bb415462ef0dd2b47e945d69aa3d591b5
Guilherme-W8 <gtavares739@gmail.com>
2024-09-12 14:06:58 -0300

    http and url

---
+++ 3-Core_Modules/4_httpWithUrl/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    const urlInfo = require('url').parse(request.url, true);
+    const name = urlInfo.query.name;
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+
+    if(!name){
+        response.end(
+        `<h1>Preencha com seu nome: </h1>
+            <form method="GET">
+                <input type="text" name="name"/>
+                <input type="submit" value="Enviar"/>
+            </form>
+        `);
+    } else {
+        response.end(`Seja bem-vindo ${name}!`);
+    }
+    
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Servidor ligado na porta ${port}`);
+});
