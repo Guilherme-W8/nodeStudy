d5bf2ff84fc31c37b6a640ce774e93800e392fe7
Guilherme-W8 <gtavares739@gmail.com>
2024-09-17 13:49:57 -0300

    Criando prompt de opcoes

--- 5-account/index.js
+++ 5-account/index.js
@@ -5,3 +5,21 @@ const inquirer = require('inquirer');
 // Interno
 const fs = require('fs');
 console.log('Hello world! - Account');
+
+function operation() {
+
+    inquirer
+        .prompt([{
+            type: 'list', // Tipo do prompt
+            name: 'action', // Nome para referencia
+            message: 'O que você deseja fazer?',
+            choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair']
+        }])
+        .then((answers) => {
+            const action = answers['action'];
+            console.log(action);
+        })
+        .catch((error) => console.log(error));
+}
+
+operation();
