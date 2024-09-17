b1a323f6beaf0825d0c57791ea829ed1bca5fbd7
Guilherme-W8 <gtavares739@gmail.com>
2024-09-17 15:03:43 -0300

    functions para criacao de conta

--- 5-account/index.js
+++ 5-account/index.js
@@ -17,9 +17,51 @@ function operation() {
         }])
         .then((answers) => {
             const action = answers['action'];
-            console.log(action);
+            
+            if(action === 'Criar conta'){
+                createAccount();
+                buildAccount();
+            }
         })
         .catch((error) => console.log(error));
 }
 
 operation();
+
+// Criar conta
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabens pela criacao da conta!'));
+    console.log(chalk.green('Defina as configuracoes da conta a seguir'));
+}
+
+// Construir conta
+function buildAccount() {
+    
+    inquirer
+        .prompt([{
+            name: 'accountName',
+            message: 'Nome da conta: '
+        }])
+        .then(answers => {
+            const accountName = answers['accountName'];
+            console.info(accountName);
+
+            if(!fs.existsSync('accounts')){
+                fs.mkdirSync('accounts');
+            } else if(fs.existsSync(`accounts/${accountName}.json`)){
+                console.log(chalk.bgRed.black('Nome de conta indisponível, escolha outro.'));
+                buildAccount();
+                return;
+            }
+
+            fs.writeFileSync(
+                `accounts/${accountName}.json`, 
+                '{"balance": 0}',
+                (error) => console.log(error)
+            ) 
+                
+            console.log(chalk.green('Conta criada com sucesso!'))
+            operation();
+        })
+        .catch(error => console.log(error));
+}
