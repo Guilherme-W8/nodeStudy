3444e0c151ca872df63d765d3626f686e73e1866
Guilherme-W8 <gtavares739@gmail.com>
2024-09-18 13:47:21 -0300

    renderizando html com express

---
+++ 6-express/3-renderHTML/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+const basePath = path.join(__dirname, 'templates');
+
+app.get('/', (request, response) => {
+  response.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
