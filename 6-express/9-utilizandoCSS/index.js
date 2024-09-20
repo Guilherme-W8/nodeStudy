2c475e2295b6262d81063c1532565c1114b7d3c5
Guilherme-W8 <gtavares739@gmail.com>
2024-09-20 14:48:48 -0300

    CSS e Express

---
+++ 6-express/9-utilizandoCSS/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const basePath = path.join(__dirname, '/templates');
+
+// Setup para ler o body
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const usersRoutes = require('./users');
+app.use('/users', usersRoutes);
+
+app.get('/', (request, response) => {
+  response.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
