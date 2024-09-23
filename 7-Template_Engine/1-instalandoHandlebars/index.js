72a10a541e3688864e84aa709f3302481bba20cb
Guilherme-W8 <gtavares739@gmail.com>
2024-09-23 14:13:54 -0300

    exphbs.engine()

--- 7-Template_Engine/1-instalandoHandlebars/index.js
+++ 7-Template_Engine/1-instalandoHandlebars/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { engine } = require('express-handlebars');  // Nova forma de importar o express-handlebars
+const exphbs = require('express-handlebars');  // Nova forma de importar o express-handlebars
 const port = 3000;
 
 const app = express();
 
 // Configurando o Handlebars
-app.engine('handlebars', engine());  // Corrigido: agora usamos 'engine()' e não 'exphbs()'
+app.engine('handlebars', exphbs.engine());  // Agora usamos 'engine()' ao inves de 'exphbs()'
 app.set('view engine', 'handlebars');
 
 app.get('/', (request, response) => {
