0e0b32bef928c14bd0895d75de736f87e2352249
Guilherme-W8 <gtavares739@gmail.com>
2024-09-23 16:43:09 -0300

    trabalhando com each no handlebars

---
+++ 7-Template_Engine/6-repeticao/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require('express-handlebars');  // Nova forma de importar o express-handlebars
+const port = 3000;
+
+const app = express();
+
+// Configurando o Handlebars
+app.engine('handlebars', exphbs.engine());  // Agora usamos 'engine()' ao inves de 'exphbs()'
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (request, response) => {
+
+  const items = ['1 - Guilherme', '2 - Laís', '3 - Sandra'];
+  response.render('dashboard', { items });
+});
+
+app.get('/', (request, response) => {
+
+  const favoriteNumber = 33;
+
+  const user = {
+    name: 'Guilherme',
+    surname: 'Correia'
+  }
+
+  const auth = true;
+  const approved = true; 
+
+  response.render('home', { user: user, favoriteNumber, auth, approved });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
