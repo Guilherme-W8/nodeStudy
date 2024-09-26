96e2737320cc47f20da2fadbde2bd70815a94a71
Guilherme-W8 <gtavares739@gmail.com>
2024-09-26 14:20:32 -0300

    Created route for list the books

--- 8-nodeSQL/1-config-mysql/index.js
+++ 8-nodeSQL/1-config-mysql/index.js
@@ -3,12 +3,6 @@ import express from 'express';
 import exphbs from 'express-handlebars';
 import mysql from 'mysql';
 
-/*
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
-*/
-
 const app = express();
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -28,12 +22,28 @@ app.post('/books/register', (request, response) => {
   connection.query(bookQuery, (error) => {
     if(error){
       console.log(error);
+      return;
     } else {
       console.log(chalk.green(`Query success!: > ${title}, ${quantity_page} <`));
     }
   });
 
-  response.redirect('/');
+  response.redirect('/books');
+});
+
+app.get('/books', (request, response) => {
+  const queryAllBooks = 'SELECT * FROM books';
+
+  connection.query(queryAllBooks, (error, dataBook) => {
+    if(error){
+      console.log(error);
+      return;
+    } else {
+      const returnedBooks = dataBook;
+      console.log('\n' + chalk.green(`Dados retornados com sucesso!\n>\n${JSON.stringify(returnedBooks)}\n<`));
+      response.render('books', { returnedBooks });
+    }
+  });
 });
 
 app.get('/', (request, response) => {
@@ -50,6 +60,7 @@ const connection = mysql.createConnection({
 connection.connect((error) => {
   if(error){
     console.log(error);
+    return;
   } else {
     app.listen(3000);
     console.log(chalk.green('Banco conectado - MySQL'));
