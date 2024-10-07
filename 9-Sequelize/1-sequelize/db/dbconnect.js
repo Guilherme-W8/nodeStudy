1ab709c188a6d6d2a4fff1980ddc0829e5eec02b
Guilherme-W8 <gtavares739@gmail.com>
2024-10-07 15:13:58 -0300

    database config

--- 9-Sequelize/1-sequelize/db/dbconnect.js
+++ 9-Sequelize/1-sequelize/db/dbconnect.js
@@ -1,15 +1,8 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('sequelizeDB', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 });
-
-try {
-  sequelize.authenticate();
-  console.log('successful connection');
-} catch(error) {
-  console.log(`Connection failed ${error}`);
-}
 
 export default sequelize;
