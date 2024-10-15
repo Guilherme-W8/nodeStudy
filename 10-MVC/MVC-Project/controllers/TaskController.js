39658ee6d207499ed6dbdc64792a903aa39118f0
guilherme-w8 <gtavares739@gmail.com>
2024-10-15 13:21:27 -0300

    new function route

--- 10-MVC/MVC-Project/controllers/TaskController.js
+++ 10-MVC/MVC-Project/controllers/TaskController.js
@@ -2,6 +2,10 @@ import Task from '../models/Tasks.js';
 
 export default class TaskController {
     static createTask(request, response){
-        response.render('tasks/creates');
+        response.render('tasks/create');
+    }
+
+    static showTasks(request, response){
+        response.render('tasks/all');
     }
 }
