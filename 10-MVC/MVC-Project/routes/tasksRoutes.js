9de3ef87458d320af713f2fe191aef04ad5af9d3
guilherme-w8 <gtavares739@gmail.com>
2024-10-15 14:44:59 -0300

    post route TaskController.saveTask()

--- 10-MVC/MVC-Project/routes/tasksRoutes.js
+++ 10-MVC/MVC-Project/routes/tasksRoutes.js
@@ -4,6 +4,8 @@ import TaskController from '../controllers/TaskController.js';
 const router = express.Router();
 
 router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.saveTask);
+
 router.get('/all', TaskController.showTasks);
 
 export default router;
