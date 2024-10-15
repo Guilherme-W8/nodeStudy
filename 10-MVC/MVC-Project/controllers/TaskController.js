e1441121cb415dde9333d5b39a782545954f922b
guilherme-w8 <gtavares739@gmail.com>
2024-10-15 14:44:14 -0300

    async function to save a task

--- 10-MVC/MVC-Project/controllers/TaskController.js
+++ 10-MVC/MVC-Project/controllers/TaskController.js
@@ -5,6 +5,19 @@ export default class TaskController {
         response.render('tasks/create');
     }
 
+    static async saveTask(request, response){
+
+        const task = {
+            title: request.body.title,
+            description: request.body.description,
+            done: false
+        }
+
+        await Task.create(task);
+        
+        response.redirect('/tasks/all');
+    }
+
     static showTasks(request, response){
         response.render('tasks/all');
     }
