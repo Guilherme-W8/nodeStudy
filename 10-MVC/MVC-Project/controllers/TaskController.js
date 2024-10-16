67a8b13db53b3466a907bfc62098ff007bd12e33
guilherme-w8 <gtavares739@gmail.com>
2024-10-16 14:01:08 -0300

    function showTasks()

--- 10-MVC/MVC-Project/controllers/TaskController.js
+++ 10-MVC/MVC-Project/controllers/TaskController.js
@@ -18,7 +18,10 @@ export default class TaskController {
         response.redirect('/tasks/all');
     }
 
-    static showTasks(request, response){
-        response.render('tasks/all');
+    static async showTasks(request, response){
+
+        const tasks = await Task.findAll({raw: true});
+
+        response.render('tasks/all', {tasks});
     }
 }
