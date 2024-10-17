966de8adf2e215090ceef0c66ea9b3d05a282849
guilherme-w8 <gtavares739@gmail.com>
2024-10-17 15:24:41 -0300

    router toggle status

--- 10-MVC/MVC-Project/routes/tasksRoutes.js
+++ 10-MVC/MVC-Project/routes/tasksRoutes.js
@@ -7,6 +7,7 @@ router.get('/add', TaskController.createTask);
 router.post('/add', TaskController.saveTask);
 router.post('/remove', TaskController.removeTask);
 router.post('/edit', TaskController.updateTaskPost);
+router.post('/updatestatus', TaskController.toggleTaskStatus);
 router.get('/edit/:id', TaskController.updateTask);
 router.get('/all', TaskController.showTasks);
 
