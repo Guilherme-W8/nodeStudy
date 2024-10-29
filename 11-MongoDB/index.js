4fe65e49ebcefe878a0f8ba0def25beabea74a56
guilherme-w8 <gtavares739@gmail.com>
2024-10-29 15:15:44 -0300

    index listen

--- 11-MongoDB/index.js
+++ 11-MongoDB/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import exphbs from 'express-handlebars'
+import exphbs from 'express-handlebars';
+import dbConnect from './db/dbConnect.js';
 
 const app = express();
-app.use(express.JSON());
+app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
@@ -10,3 +11,5 @@ app.use(express.static('public'));
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+app.listen(3000);
