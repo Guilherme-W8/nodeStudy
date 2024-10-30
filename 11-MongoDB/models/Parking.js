39b600260198997c8188830037ee7299dacc8f0f
guilherme-w8 <gtavares739@gmail.com>
2024-10-30 09:14:28 -0300

    Model Parking

--- 11-MongoDB/models/Parking.js
+++ 11-MongoDB/models/Parking.js
@@ -1,1 +1,21 @@
+import dbConnect from '../db/dbConnect.js';
+
+class Parking {
+    constructor(location, spotQuantity, description) {
+        this.location = location;
+        this.spotQuantity = spotQuantity;
+        this.description = description;
+    }
+
+    save() {
+        const parkings = dbConnect.db().collection('parkings').insertOne({
+            location: this.location,
+            spotQuantity: this.spotQuantity,
+            description: this.description
+        });
+
+        return parkings;
+    }
+}
+
 export default Parking;
