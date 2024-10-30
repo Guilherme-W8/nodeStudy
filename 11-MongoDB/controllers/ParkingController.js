ac9916f6d4eb4e92cc4a2bd6d812e23bd14e86cb
guilherme-w8 <gtavares739@gmail.com>
2024-10-30 09:34:43 -0300

    render form create

--- 11-MongoDB/controllers/ParkingController.js
+++ 11-MongoDB/controllers/ParkingController.js
@@ -4,4 +4,8 @@ export default class Parking {
     static showParkings(request, response) {
         response.render('parkings/all');
     }
+
+    static createParkingForm(request, response) {
+        response.render('parkings/create');
+    }
 }
--- 11-MongoDB/routes/parkingRoutes.js
+++ 11-MongoDB/routes/parkingRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ParkingController from '../controllers/ParkingController.js';
 const router = express.Router();
 
+router.get('/create', ParkingController.createParkingForm);
 router.get('/', ParkingController.showParkings);
 
 export default router;
