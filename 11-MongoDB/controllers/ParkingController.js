be3e796376a298c6719aac80c923caad2fd3423e
guilherme-w8 <gtavares739@gmail.com>
2024-10-30 15:08:49 -0300

    post parking

--- 11-MongoDB/controllers/ParkingController.js
+++ 11-MongoDB/controllers/ParkingController.js
@@ -1,11 +1,23 @@
-// import Parking from '../models/Parking.js';
+import Parking from '../models/Parking.js';
 
-export default class Parking {
+export default class ParkingController {
     static showParkings(request, response) {
         response.render('parkings/all');
     }
 
     static createParkingForm(request, response) {
         response.render('parkings/create');
+    }
+
+    static createParkingPost(request, response) {
+        const location = request.body.location;
+        const spotQuantity = request.body.spotQuantity;
+        const description = request.body.description;
+
+        const parking = new Parking(location, spotQuantity, description);
+
+        parking.save();
+
+        response.redirect('/parking');
     }
 }
--- 11-MongoDB/routes/parkingRoutes.js
+++ 11-MongoDB/routes/parkingRoutes.js
@@ -3,6 +3,7 @@ import ParkingController from '../controllers/ParkingController.js';
 const router = express.Router();
 
 router.get('/create', ParkingController.createParkingForm);
+router.post('/create', ParkingController.createParkingPost);
 router.get('/', ParkingController.showParkings);
 
 export default router;
