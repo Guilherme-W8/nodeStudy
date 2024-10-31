0c0e791b32afde9c740cdc51943e4913b07da0cf
guilherme-w8 <gtavares739@gmail.com>
2024-10-31 15:51:58 -0300

    get all parkings in database

--- 11-MongoDB/models/Parking.js
+++ 11-MongoDB/models/Parking.js
@@ -1,7 +1,9 @@
 import dbConnect from '../db/dbConnect.js';
 
 class Parking {
-    constructor(location, spotQuantity, description) {
+    constructor(image, name, location, spotQuantity, description) {
+        this.image = image;
+        this.name = name;
         this.location = location;
         this.spotQuantity = spotQuantity;
         this.description = description;
@@ -9,10 +11,18 @@ class Parking {
 
     save() {
         const parkings = dbConnect.db().collection('parkings').insertOne({
+            image: this.image,
+            name: this.name,
             location: this.location,
             spotQuantity: this.spotQuantity,
             description: this.description
         });
+
+        return parkings;
+    }
+
+    static getParkings() {
+        const parkings = dbConnect.db().collection('parkings').find().toArray();
 
         return parkings;
     }
