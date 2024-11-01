5cae06b81f767c4a912826b293d9f53c439c5a78
guilherme-w8 <gtavares739@gmail.com>
2024-11-01 13:39:59 -0300

    router showParking

--- 11-MongoDB/routes/parkingRoutes.js
+++ 11-MongoDB/routes/parkingRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/create', ParkingController.createParkingForm);
 router.post('/create', ParkingController.createParkingPost);
 router.get('/', ParkingController.showParkings);
+router.get('/:id', ParkingController.showParking);
 
 export default router;
