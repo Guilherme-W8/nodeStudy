0b3f5938c226ee4233da7e3eb1b97bc49d1a1c4a
guilherme-w8 <gtavares739@gmail.com>
2024-11-02 14:20:32 -0300

    routes and views

--- 11-MongoDB/routes/parkingRoutes.js
+++ 11-MongoDB/routes/parkingRoutes.js
@@ -7,6 +7,8 @@ router.post('/create-post', ParkingController.createParkingPost);
 
 router.post('/remove/:id', ParkingController.removeParking);
 
+router.get('/edit/:id', ParkingController.editParkingForm);
+
 router.get('/show/:id', ParkingController.showParking);
 router.get('/', ParkingController.showParkings);
 
