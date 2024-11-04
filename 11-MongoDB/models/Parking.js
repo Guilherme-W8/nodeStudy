7c5bb885f06ad7b9c1c9abc710ba62e6e461c863
guilherme-w8 <gtavares739@gmail.com>
2024-11-03 23:17:44 -0300

    Parking Schema Mongoose

--- 11-MongoDB/models/Parking.js
+++ 11-MongoDB/models/Parking.js
@@ -1,58 +1,15 @@
-import dbConnect from '../db/dbConnect.js';
-import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
-class Parking {
-    constructor(name, location, spotQuantity, description, image) {
-        this.name = name;
-        this.location = location;
-        this.spotQuantity = spotQuantity;
-        this.description = description;
-        this.image = image;
-    }
-
-    save() {
-        const parkings = dbConnect.db().collection('parkings').insertOne({
-            name: this.name,
-            location: this.location,
-            spotQuantity: this.spotQuantity,
-            description: this.description,
-            image: this.image
-        });
-
-        return parkings;
-    }
-
-    static async getParkings() {
-        const parkings = await dbConnect.db().collection('parkings').find().toArray();
-
-        return parkings;
-    }
-
-    static async getParkingById(id) {
-        const parking = await dbConnect.db().collection('parkings').findOne({
-            _id: new ObjectId(id),
-        });
-
-        return parking;
-    }
-
-    static async removeParkingById(id) {
-        await dbConnect.db().collection('parkings').deleteOne({
-            _id: new ObjectId(id),
-        });
-
-        return;
-    }
-
-    updateParking(id) {
-        dbConnect.db().collection('parkings').updateOne({
-            _id: new ObjectId(id)
-        }, {
-            $set: this
-        });
-
-        return;
-    }
-}
+const Parking = mongoose.model(
+    'Parking',
+    new Schema({
+        name: { type: String, required: true },
+        location: { type: String, required: true },
+        spotQuantity: { type: Number, required: true },
+        description: { type: String, required: true },
+        image: { type: String },
+    })
+);
 
 export default Parking;
