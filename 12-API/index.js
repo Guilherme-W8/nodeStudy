c3062dab88fed782a3ea9e3e8b70941cffdfdc35
guilherme-w8 <gtavares739@gmail.com>
2024-11-05 15:37:52 -0300

    post endpoint and status code

--- 12-API/index.js
+++ 12-API/index.js
@@ -9,8 +9,23 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 // Rotas - Endpoints
+app.post('/createcompany', (request, response) => {
+    const name = request.body.name;
+    const employeeQuantity = request.body.employeeQuantity;
+
+    // Error status
+    if (!name) {
+        return response.status(422).json({ message: 'Campo nome é obrigatório!' });
+    }
+
+    console.log(name);
+    console.log(employeeQuantity);
+
+    return response.status(201).json({ message: `Empresa ${name} adicionada com sucesso!` });
+});
+
 app.get('/', (request, response) => {
-    response.json({
+    return response.json({
         message: 'Hello World! - Primeira rota da API'
     });
 });
